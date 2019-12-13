b8a61634a7cd81dd420b5b71d337fcece3100d2e
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-13 18:54:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function theBeatlesPlay(musicians, instruments)
 {
   var x = [];
-  var counter = 1;
-  for(var i=1; i < counter; i++)
+  var counter = length.musicians;
+  for(var i=1; i < counter+1; i++)
   {
     
   }
