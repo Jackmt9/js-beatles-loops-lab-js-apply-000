7fd2ee34ef6faed264531b10a2a8383492e38303
dandy-clone-9171 <jackmt9@gmail.com>
2019-12-13 19:05:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments)
   var counter = musicians.length;
   for(var i=0; i < counter; i++)
   {
-    var temp = alert(musicians[i]) + " " + alert(instruments[i]);
+    var temp = musicians[i] + " " + instruments[i];
     x.push(temp);
   }
   return x;
@@ -13,9 +13,9 @@ function theBeatlesPlay(musicians, instruments)
 function johnLennonFacts(facts)
 {
   var i=0;
-  while(i < facts.length)
+  while(i <= facts.length)
   {
-   alert(musicians) 
-   i++ 
+   facts[i]=facts[i]+"!!!" 
+   i++
   }
 }
